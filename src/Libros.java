1b5812938eaa1bbfdf1395dee13dfa4529f86702
MateMar04 <mateo.marchisone@gmail.com>
2021-10-08 23:38:41 -0300

    Changing label information in Libros and Revistas

--- src/Libros.java
+++ src/Libros.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 import java.util.Vector;
 
 public class Libros extends javax.swing.JFrame {
     private JPanel panel1;
     private JComboBox libros_combo;
+    private JLabel title_info;
+    private JLabel autor_info;
+    private JLabel categoria_info;
+    private JLabel editorial_info;
 
 
     public Libros(Biblioteca bib) {
@@ -15,12 +22,25 @@ public class Libros extends javax.swing.JFrame {
 
         Vector comboBox_items = new Vector();
 
+        List<Libro> libros = bib.getLibros();
 
-        for (Libro libro : bib.getLibros()) {
+        for (Libro libro : libros) {
             comboBox_items.add(libro.getTitulo());
         }
 
         libros_combo.setModel(new DefaultComboBoxModel(comboBox_items));
         libros_combo.setSelectedIndex(-1);
+
+        libros_combo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Libro libro = libros.get(libros_combo.getSelectedIndex());
+                title_info.setText(libro.getTitulo());
+                autor_info.setText(libro.getTitulo());
+                editorial_info.setText(libro.getEditorial().toString());
+                categoria_info.setText(libro.getCategoria().toString());
+
+            }
+        });
     }
 }
--- src/Revistas.java
+++ src/Revistas.java
@@ -1,10 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
 import java.util.Vector;
 
 public class Revistas extends javax.swing.JFrame {
     private JComboBox revistas_combo;
     private JPanel panel1;
+    private JLabel title_info;
+    private JLabel autor_info;
+    private JLabel editorial_info;
+    private JLabel categoria_info;
 
 
     public Revistas(Biblioteca bib) {
@@ -15,6 +22,8 @@ public class Revistas extends javax.swing.JFrame {
 
         Vector comboBox_items = new Vector();
 
+        List<Revista> revistas = bib.getRevistas();
+
 
         for (Revista revista : bib.getRevistas()) {
             comboBox_items.add(revista.getTitulo());
@@ -22,6 +31,18 @@ public class Revistas extends javax.swing.JFrame {
 
         revistas_combo.setModel(new DefaultComboBoxModel(comboBox_items));
         revistas_combo.setSelectedIndex(-1);
+
+        revistas_combo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Revista revista = revistas.get(revistas_combo.getSelectedIndex());
+                title_info.setText(revista.getTitulo());
+                autor_info.setText(revista.getTitulo());
+                editorial_info.setText(revista.getEditorial().toString());
+                categoria_info.setText(revista.getCategoria().toString());
+
+            }
+        });
     }
 }
 
